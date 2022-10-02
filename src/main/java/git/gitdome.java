d9f538feeff6316417969d0f17d0c9f76cfe5297
yx <2745223955@qq.com>
2022-10-02 14:24:58 +0800

    hot-fix commit 5

--- src/main/java/git/gitdome.java
+++ src/main/java/git/gitdome.java
@@ -7,9 +7,11 @@ public class gitdome {
         s1();
         s2();
         s3();
+        s4();
     }
     public static void s(){System.out.println("hello git1");}
     public static void s1(){System.out.println("hello git2");}
     public static void s2(){System.out.println("hello git3");}
     public static void s3(){System.out.println("hello git4");}
+    public static void s4(){System.out.println("hello git5");}
 }
