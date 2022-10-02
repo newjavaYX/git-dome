e9ecc82578edbebd55915aa76d60f99d5d735e28
yx <2745223955@qq.com>
2022-10-02 14:18:42 +0800

    first commit test 5

--- src/main/java/git/gitdome.java
+++ src/main/java/git/gitdome.java
@@ -4,17 +4,12 @@ public class gitdome {
     public static void main(String[] args) {
         System.out.println("hello git ");
         s();
+        s1();
+        s2();
+        s3();
     }
-    public static void s(){
-        System.out.println("hello git2");
-    }
-    public static void s2(){
-        System.out.println("hello git3");
-    }
-    public static void s3(){
-        System.out.println("hello git4");
-    }
-    public static void s1(){
-        System.out.println("hello git3");
-    }
+    public static void s(){System.out.println("hello git1");}
+    public static void s1(){System.out.println("hello git2");}
+    public static void s2(){System.out.println("hello git3");}
+    public static void s3(){System.out.println("hello git4");}
 }
