9e5425f5f92a4e0caeee7aae2f6e17a9b329d136
yx <2745223955@qq.com>
2022-10-02 10:26:46 +0800

    first commit test 2

--- src/main/java/git/gitdome.java
+++ src/main/java/git/gitdome.java
@@ -3,5 +3,9 @@ package git;
 public class gitdome {
     public static void main(String[] args) {
         System.out.println("hello git ");
+        s();
+    }
+    public static void s(){
+        System.out.println("hello git2");
     }
 }
